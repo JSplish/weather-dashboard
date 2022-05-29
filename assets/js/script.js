3749b1daf38fe9671806043afabf15c436b514e5
Jordan <jordan.m.edginton@gmail.com>
2022-05-29 14:41:25 -0600

    saved searches and code cleanup, need to fix 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
 function initPage() {
     const city = document.getElementById("enter-city");
     const search = document.getElementById("search-button")
-    const weatherToday = document.getElementById("today-weather");
     const currentPicture = document.getElementById("current-pic");
     const currentTemp = document.getElementById("temperature")
     const currentHumidity = document.getElementById("humidity");
+    const currentWind = document.getElementById("wind-spped");
     const currentUv = document.getElementById("UV-index");
-    const fiveday = document.getElementById("fiveday-header");
     const clearHistory = document.getElementById("clear-history");
+    const historyEl = document.getElementById("history");
+    const nameEl = document.getElementById("city-name");
+    var weatherToday = document.getElementById("today-weather");
+    var fiveday = document.getElementById("fiveday-header");
     let history = JSON.parse(localStorage.getItem("search")) || [];
 
 
@@ -59,7 +62,7 @@ function initPage() {
                     });
 
                     // 5 day forecast
-                    let cityId = res.data.id;
+                    let cityID = res.data.id;
                     let forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&appid=" + APIKey;
                     axios.get(forecastUrl)
                     .then(function (res) {
@@ -77,6 +80,18 @@ function initPage() {
                             forecastDateEl.setAttribute("class", "mt-3 mb-0 forecast-date");
                             forecastDateEl.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
                             forecastEl[i].append(forecastDateEl);
+
+                            // Icon for current weather
+                            const forecastWeather = document.createElement("img");
+                            forecastWeather.setAttribute("src", "https://openweathermap.org/img/wn/" + response.data.list[forecastIndex].weather[0].icon + "@2x.png");
+                            forecastWeather.setAttribute("alt", response.data.list[forecastIndex].weather[0].description);
+                            forecastEl[i].append(forecastWeather);
+                            const forecastTemp = document.createElement("p");
+                            forecastTemp.innerHTML = "Temp: " + k2f(response.data.list[forecastIndex].main.temp) + " &#176F";
+                            forecastEl[i].append(forecastTemp);
+                            const forecastHumidity = document.createElement("p");
+                            forecastHumidity.innerHTML = "Humidity: " + response.data.list[forecastIndex].main.humidity + "%";
+                            forecastEl[i].append(forecastHumidity);
                         }
                     })
             });
@@ -96,4 +111,32 @@ function initPage() {
         history = [];
         displayHistory();
     })
+
+    function k2f(K) {
+        return Math.floor((K - 273.15) * 1.8 + 32);
+    }
+
+    //Saved Searches
+    function displayHistory() {
+        historyEl.innerHTML = "";
+        for (let i = 0; i < history.length; i++) {
+            const historyItem = document.createElement("input");
+            historyItem.setAttribute("type", "text");
+            historyItem.setAttribute("readonly", "true");
+            historyItem.setAttribute("class", "form-control d-block bg-white");
+            historyItem.setAttribute("value", history[i]);
+            historyItem.addEventListener("click", function() {
+                getWeather(historyItem.value);
+            })
+            historyEl.append(historyItem);
+
+        }
+    }
+
+    displayHistory();
+    if(history.length > 0) {
+        getWeather(history[history.length - 1]);
+    }
 }
+
+initPage();
