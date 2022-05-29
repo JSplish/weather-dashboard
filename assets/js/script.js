06a741b4ddfce914c51e2c90fbc82f63c162f820
Jordan <jordan.m.edginton@gmail.com>
2022-05-29 13:44:20 -0600

    assign api key, get current weather and display

---
+++ assets/js/script.js
@@ -0,0 +1,60 @@
+function initPage() {
+    const cityEl = document.getElementById("enter-city");
+    const searchEl = document.getElementById("search-button")
+    const weatherToday = document.getElementById("today-weather");
+    const currentPicture = document.getElementById("current-pic");
+    const currentTemp = document.getElementById("temperature")
+    const currentHumidity = document.getElementById("humidity");
+    const currentUv = document.getElementById("UV-index");
+
+    // Unique API key
+    const APIKey = "e7c9a6c1e200e70e07d3d9c7208425ac";
+
+    function getWeather(cityName) {
+
+        // Get current weather from API
+        let queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+        axios.get(queryUrl)
+            .then(function (res) {
+                weatherToday.classList.remove("d-none");
+
+                // Display current weather
+                const currentDate = new Date(res.data.dt * 1000);
+                const day = currentDate.getDate();
+                const month = currentDate.getMonth() + 1;
+                const year = currentDate.getFullYear();
+                nameEl.innerHTML = res.data.name + " (" + month + "/" + day + "/" + year + ") ";
+                let weatherPicture = res.data.weather[0].icon;
+                currentPicture.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherPicture + "@2x.png");
+                currentPicture.setAttribute("alt", res.data.weather[0].description);
+                currentTemp.innerHTML = "Temperature: " + k2f(res.data.main.temp) + " &#176F";
+                currentHumidity.innerHTML = "Humidity: " + res.data.main.humidity + "%";
+                currentWind.innerHTML = "Wind Speed: " + res.data.wind.speed + "MPH";
+
+                // UV Index
+                let latitude = res.data.coord.lat;
+                let longitude = res.data.coord.lon;
+                let UV = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIKey + "&cnt=1";
+                axios.get(UV)
+                    .then(function (res) {
+                        let UVIndex = document.createElement("span");
+
+                        // UV Index good=green, decent=yellow, bad=red
+                        if(res.data[0].value < 4) {
+                            UVIndex.setAttribute("class", "badge badge-success");
+                        }
+                        else if(res.data[0].value < 8) {
+                            UVIndex.setAttribute("class", "badge badge-warning");
+                        }
+                        else {
+                            UVIndex.setAttribute("class", "badge badge-danger");
+                        }
+                        UVIndex.innerHTML = res.data[0].value;
+                        currentUv.innerHTML = "UV Index: ";
+                        currentUv.append(UVIndex);
+                    });
+
+
+            })
+    }
+}
